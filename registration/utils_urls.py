1757352fa0128b5f5535fb87d83f1884ff068f82
aelsir <ahmed.elsir.khalfalla@gmail.com>
2022-08-19 16:46:42 +0200

    plot the registration per week instead of per month

--- registration/utils_urls.py
+++ registration/utils_urls.py
@@ -2,7 +2,7 @@ from urllib import request, response
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from .models import Registration, Student
-from django.db.models.functions import TruncMonth
+from django.db.models.functions import TruncWeek
 from django.db.models import Count
 from django.db.models import Q
 from django.http import HttpResponse
@@ -17,7 +17,6 @@ def registrations_list(request):
         return render(request, "registration/registrations_list.html", {
             "all_registrations": all_registrations,
         })
-
 
 # download the students registration list in a CSV
 @staff_member_required
@@ -38,9 +37,9 @@ def dashboard(request):
     data_true_count = []
     data_total_count = []
 
-    queryset = Registration.objects.annotate(month=TruncMonth('created_at')).values('month').annotate(true_count = Count('is_enroll', filter=Q(is_enroll=True))).values('month', 'true_count').annotate(total_count = Count('is_enroll')).values('month', 'true_count', 'total_count')
+    queryset = Registration.objects.annotate(week=TruncWeek('created_at')).values('week').annotate(true_count = Count('is_enroll', filter=Q(is_enroll=True))).values('week', 'true_count').annotate(total_count = Count('is_enroll')).values('week', 'true_count', 'total_count').order_by('week')
     for register in queryset:
-        labels.append(register['month'].strftime("%b"))
+        labels.append(register['week'].strftime("%a %d/%m"))
         data_true_count.append(register['true_count'])
         data_total_count.append(register['total_count'])
     return render(request, "registration/dashboard.html", {
