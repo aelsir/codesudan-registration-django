4f9b6656b35490fd05470fe27082ae4349d7250a
aelsir <ahmed.elsir.khalfalla@gmail.com>
2023-01-29 22:18:22 +0000

    finishing the ranking

--- ranking/admin.py
+++ ranking/admin.py
@@ -6,7 +6,7 @@ from .models import Developer
 @admin.register(Developer)
 class DeveoperAdmin(admin.ModelAdmin):
     list_display = ('name_arabic', 'rank')
-    ordering = ('-rank',)
+    ordering = ('rank',)
     search_fields = ['name_arabic', 'name_english', 'rank']
 
 
