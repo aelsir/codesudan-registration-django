fac450aa7ba5ca5424a62cdd84a52827df16a511
aelsir <ahmed.elsir.khalfalla@gmail.com>
2023-01-29 22:18:22 +0000

    finishing the ranking

--- ranking/models.py
+++ ranking/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Developer(models.Model):
+
+    class Countries(models.TextChoices):
+        SUDAN = 'sd', 'السودان'
     name_arabic = models.CharField(max_length=64)
     name_english = models.CharField(max_length=64)
+    country = models.CharField(max_length=2, choices=Countries.choices, default=Countries.SUDAN)
     rank = models.SmallIntegerField()
-    facebook_url = models.URLField()
-    linkedin_url = models.URLField()
-    github_url = models.URLField()
+    followers = models.IntegerField()
+    facebook_url = models.URLField(blank=True)
+    linkedin_url = models.URLField(blank=True)
+    github_url = models.URLField(blank=True)
+    twitter_url = models.URLField(blank=True)
 
     class Meta:
         permissions = [
