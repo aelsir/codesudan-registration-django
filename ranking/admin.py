2c545de9b2fcb3abef2342419750e8897e14439e
aelsir <ahmed.elsir.khalfalla@gmail.com>
2023-01-31 11:20:27 +0000

    chanigng the ordering in admin

--- ranking/admin.py
+++ ranking/admin.py
@@ -6,7 +6,7 @@ from .models import Developer
 @admin.register(Developer)
 class DeveoperAdmin(admin.ModelAdmin):
     list_display = ('name_arabic', 'contribs')
-    ordering = ('contribs',)
+    ordering = ('-contribs',)
     search_fields = ['name_arabic', 'name_english', 'contribs']
 
 
