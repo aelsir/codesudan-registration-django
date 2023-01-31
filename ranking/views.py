50737e601badcd508f0d0f6ec0c9136e0673e2f1
aelsir <ahmed.elsir.khalfalla@gmail.com>
2023-01-31 18:51:57 +0000

    adding ranking to the main page

--- ranking/views.py
+++ ranking/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from .models import Developer
@@ -8,7 +6,7 @@ from .models import Developer
 
 # Create your views here.
 
-class DevelopersListView(LoginRequiredMixin, ListView):
+class DevelopersListView(ListView):
     model = Developer
     template_name = 'developers_list.html'
     context_object_name = 'developers'
